9fcd4aa4cfee89ac90a9c4e44d251be12bc7f0f3
edft56 <edft56@hotmail.com>
2022-08-30 22:32:11 +0300

    Changed FSM style

--- instruction_unit.sv
+++ instruction_unit.sv
@@ -11,7 +11,7 @@
 module instruction_unit
                         import tpu_package::*;    
                         (
-                            input  clk_i,rst_i,
+                            input  clk_i,rstN_i,
                             input [INSTR_SIZE-1:0] instruction_i,
                             input  write_i,
                             input  read_i,
@@ -21,7 +21,14 @@ module instruction_unit
                             decoded_instr_t decoded_instruction_o
                         );
 
-    enum logic [1:0] {RESET, EMPTY, STEADY, FULL} iq_state;
+    typedef enum logic [1:0] {
+                                RESET,
+                                EMPTY,
+                                STEADY,
+                                FULL
+                                } iq_states_t;
+
+    iq_states_t iq_state, next_iq_state;
 
     decoded_instr_t decoded_instruction_fifo[32];
 
@@ -33,13 +40,36 @@ module instruction_unit
 
     initial read_idx_q  = '0;
     initial write_idx_q = '0;
-    initial iq_state = EMPTY;
-    initial iq_empty_o = '1;
-    initial iq_full_o = '0;
+    initial iq_state    = RESET;
+    initial iq_empty_o  = '1;
+    initial iq_full_o   = '0;
 
     always_comb begin
         next_read_idx  = (read_i) ? read_idx_q + 1 : read_idx_q;
         next_write_idx = (write_i) ? write_idx_q + 1 : write_idx_q;
+    end
+
+    always_ff @( posedge clk_i, negedge rstN_i ) begin
+        if(rstN_i)  iq_state = RESET;
+        else        iq_state = next_iq_state;
+    end
+
+    always_comb begin
+        case(iq_state)
+            RESET: begin
+                next_iq_state = EMPTY;
+            end
+            EMPTY: begin
+                next_iq_state = (write_i) ? STEADY : EMPTY;
+            end
+            STEADY: begin
+                next_iq_state =  ( write_i & next_write_idx == next_read_idx) ? FULL :
+                                 ( (read_i & next_write_idx == next_read_idx) ? EMPTY : STEADY );
+            end
+            FULL: begin
+                next_iq_state = (read_i) ? STEADY : FULL;
+            end
+        endcase
     end
 
     always_ff @( posedge clk_i ) begin
@@ -71,13 +101,12 @@ module instruction_unit
             end
             EMPTY: begin
                 write_idx_q <= next_write_idx;
-                read_idx_q  <= next_read_idx;
+                //read_idx_q  <= next_read_idx;
 
                 iq_empty_o  <= '1;
                 
                 if (write_i) begin
                     iq_empty_o  <= '0;
-                    iq_state    <= STEADY;
                 end
             end
             STEADY: begin
@@ -86,29 +115,97 @@ module instruction_unit
         
                 if ( write_i & next_write_idx == next_read_idx) begin
                     iq_full_o  <= '1;
-                    iq_state   <= FULL;
                 end
 
                 if ( read_i & next_write_idx == next_read_idx) begin
                     iq_empty_o  <= '1;
-                    iq_state   <= EMPTY;
                 end
             
             end
             FULL: begin
-                write_idx_q <= next_write_idx;
+                //write_idx_q <= next_write_idx;
                 read_idx_q  <= next_read_idx;
 
                 iq_full_o <= '1;
                 
                 if (read_i) begin
                     iq_full_o <= '0;
-                    iq_state <= STEADY;
                 end
             end
         endcase
         
     end
+
+
+
+
+    // always_ff @( posedge clk_i ) begin
+    //     decoded_instruction_o           <= decoded_instruction_fifo[read_idx_q];
+
+    //     case({write_i,instruction_i[ 3: 0]})
+    //         5'b10001: begin
+    //             decoded_instruction_fifo[write_idx_q].MAC_op                        <= 3'b010;
+    //             decoded_instruction_fifo[write_idx_q].V_dim                         <= instruction_i[11: 4];
+    //             decoded_instruction_fifo[write_idx_q].U_dim                         <= instruction_i[19:12];
+    //             decoded_instruction_fifo[write_idx_q].ITER_dim                      <= instruction_i[27:20];
+    //             decoded_instruction_fifo[write_idx_q].unified_buffer_start_addr_rd  <= instruction_i[39:28];
+    //             decoded_instruction_fifo[write_idx_q].unified_buffer_start_addr_wr  <= instruction_i[51:40];
+
+    //             decoded_instruction_fifo[write_idx_q].V_dim1                        <= instruction_i[11: 4] - 1;
+    //             decoded_instruction_fifo[write_idx_q].U_dim1                        <= instruction_i[19:12] - 1;
+    //             decoded_instruction_fifo[write_idx_q].ITER_dim1                     <= instruction_i[27:20] - 1;
+    //         end
+    //         default: begin
+    //         end
+    //     endcase
+
+    //     case(iq_state)
+    //         RESET: begin
+    //             iq_empty_o  <= '1;
+    //             iq_full_o   <= '1;
+    //             write_idx_q <= '0;
+    //             read_idx_q  <= '0;
+    //         end
+    //         EMPTY: begin
+    //             write_idx_q <= next_write_idx;
+    //             //read_idx_q  <= next_read_idx;
+
+    //             iq_empty_o  <= '1;
+                
+    //             if (write_i) begin
+    //                 iq_empty_o  <= '0;
+    //                 iq_state    <= STEADY;
+    //             end
+    //         end
+    //         STEADY: begin
+    //             write_idx_q <= next_write_idx;
+    //             read_idx_q  <= next_read_idx;
+        
+    //             if ( write_i & next_write_idx == next_read_idx) begin
+    //                 iq_full_o  <= '1;
+    //                 iq_state   <= FULL;
+    //             end
+
+    //             if ( read_i & next_write_idx == next_read_idx) begin
+    //                 iq_empty_o  <= '1;
+    //                 iq_state   <= EMPTY;
+    //             end
+            
+    //         end
+    //         FULL: begin
+    //             //write_idx_q <= next_write_idx;
+    //             read_idx_q  <= next_read_idx;
+
+    //             iq_full_o <= '1;
+                
+    //             if (read_i) begin
+    //                 iq_full_o <= '0;
+    //                 iq_state <= STEADY;
+    //             end
+    //         end
+    //     endcase
+        
+    // end
 
 endmodule
 
